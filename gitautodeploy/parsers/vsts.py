393b39f575683ed32493641ec4cd8791d681001a
normisg <normisg@gmail.com>
2018-07-03 10:51:58 +0300

    Update vsts.py

--- gitautodeploy/parsers/vsts.py
+++ gitautodeploy/parsers/vsts.py
@@ -1,5 +1,5 @@
+import re
 from .base import WebhookRequestParserBase
-
 
 class VstsRequestParser(WebhookRequestParserBase):
 
@@ -9,12 +9,16 @@ class VstsRequestParser(WebhookRequestParserBase):
         data = json.loads(request_body)
 
         repo_urls = []
+        pattern = re.compile(r"^https:\/\/(?P<host>\w+)\.visualstudio\.com\/(?P<project>\w+)\/_git\/(?P<code>\w+)$");
 
         action.log_debug("Received event from VSTS")
 
         try:
             url = data['resource']['repository']['remoteUrl']
-            repo_urls.append(url)
+            parts = re.findall(pattern, url)
+            ssh_url = 'ssh://%s@vs-ssh.visualstudio.com:22/%s/_ssh/%s' % (result[0][0], result[0][1], result[0][2])
+            
+            repo_urls.append(ssh_url)
             repo_configs = self.get_matching_repo_configs(repo_urls, action)
 
             return repo_configs
